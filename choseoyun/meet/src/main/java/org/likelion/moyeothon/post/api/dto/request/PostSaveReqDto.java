5f47235d591cc4b1bb2a06e3a3018940915b1c53
seoyun <seoyun-cho@naver.com>
2024-11-02 01:59:29 +0900

    수정2

--- choseoyun/meet/src/main/java/org/likelion/moyeothon/post/api/dto/request/PostSaveReqDto.java
+++ choseoyun/meet/src/main/java/org/likelion/moyeothon/post/api/dto/request/PostSaveReqDto.java
@@ -2,7 +2,7 @@ package org.likelion.moyeothon.post.api.dto.request;
 
 public record PostSaveReqDto(
         //작성자 아이디, 제목, 내용, 영화관, 지역, 오픈 채팅 링크
-        Long userId,
+        Long memberId,
         String title,
         String contents,
         String theaterName,
